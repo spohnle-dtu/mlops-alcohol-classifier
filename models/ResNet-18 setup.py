e9fedb3e746298813c5f50c506fac49bba5d70d8
s204151 <s204151@student.dtu.dk>
2026-01-09 13:42:21 +0100

    ResNet18 setup code
    Added ResNet-18 example code

---
+++ models/ResNet-18 setup.py
@@ -0,0 +1,22 @@
+from torchvision.io import decode_image
+from torchvision.models import resnet18, ResNet18_Weights
+
+img = decode_image(r"C:\Users\Damho\Desktop\dataset MLops\archive\Alcohol_Dataset\whiskey\whiskey-50.jpg")
+
+# Step 1: Initialize model with the best available weights
+weights = ResNet18_Weights.DEFAULT
+model = resnet18(weights=weights)
+model.eval()
+
+# Step 2: Initialize the inference transforms
+preprocess = weights.transforms()
+
+# Step 3: Apply inference preprocessing transforms
+batch = preprocess(img).unsqueeze(0)
+
+# Step 4: Use the model and print the predicted category
+prediction = model(batch).squeeze(0).softmax(0)
+class_id = prediction.argmax().item()
+score = prediction[class_id].item()
+category_name = weights.meta["categories"][class_id]
+print(f"{category_name}: {100 * score:.1f}%")
